2e6ffd8eacc2292a2531b693869e2185dade22ea
mechanicworld <47930174+mechanicworld@users.noreply.github.com>
2022-04-02 02:01:32 +0300

    Add listen server and port

--- clean-blog-project/app.js
+++ clean-blog-project/app.js
@@ -5,4 +5,11 @@ const app = express();
 const port = 3000;
 app.get('/', ((req, res) => {
   const blog = { id: 1, title: "Blog Title", description: "Blog description" }
+  res.send(blog)
+
 }))
+
+
+app.listen(port, () => {
+  console.log(`Server is running at port:${port}`)
+})
